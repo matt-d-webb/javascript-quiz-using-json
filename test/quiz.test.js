530f9491a676c5c5954b5cbe8bd5ec4638b403dd
Matthew Webb <matt.d.webb@icloud.com>
2017-03-20 22:46:15 +0000

    created all test cases

--- test/quiz.test.js
+++ test/quiz.test.js
@@ -29,26 +29,80 @@ describe('Json Quiz', function() {
     quiz = window.Quiz.__TEST__;
   })
 
-  it('Should be defined', function() {
-      expect(quiz).to.be.a('object');
-  });
+  describe('Templates', function() {
 
-  it('Should have 16 private methods', function() {
-      expect(Object.keys(quiz).length).to.equal(16);
+    it('Should be defined', function() {
+        expect(quiz).to.be.a('object');
+    });
+
+    it('Should have 16 private methods', function() {
+        expect(Object.keys(quiz).length).to.equal(16);
+    });
+
   });
 
   describe('Templates', function() {
+
     it('questionTemplate() should return a string from', function() {
 
-        var data = { "question": "Valid Question 1 - four options",
-                        "info": "Information 1",
-                        "options":["Option 1","Option 2","Option 3","Option 4"],
-                        "scores":[4,3,1,2]};
+        var data = {
+          question: "Valid Question 1 - four options",
+          info: "Information 1",
+          options:["Option 1","Option 2","Option 3","Option 4"],
+          scores:[4,3,1,2]
+        };
 
         var str = quiz.questionTemplate(data.question,data.options);
 
         expect(str).to.be.a('string');
     });
+
+    it('getTemplate() should return a valid template', function() {
+
+      var template = quiz.getTemplate(data);
+
+    });
+
+    it('resultMessage() should return a summarised html message with the correct data', function() {
+
+    });
+
+    it('informationTemplate() should return html template with the correct data', function() {
+
+    });
+
+    it('renderTemplate() should add the correct html to the DOM', function() {
+
+    });
+
+  });
+
+  describe('Core functionality', function() {
+
+    it('init() should initialse the quiz with the correct first question', function() {
+
+    });
+
+    it('start() should update the question array', function() {
+
+    });
+
+    it('end() should clear all question array data', function() {
+
+    });
+
+    it('bindSubmit() should add an event listener to the DOM', function() {
+
+    });
+
+    it('nextQuestion() should iterate through the array of questions', function() {
+
+    });
+
+  });
+
+  describe('Utilities', function() {
+
     it('extend() should return an object with merged properties', function() {
 
         var obj1 = { name: 'test', val: 2 };
@@ -61,7 +115,23 @@ describe('Json Quiz', function() {
         expect(extended.val).to.equal(2);
         expect(extended.other).to.equal(4);
     });
-  });
 
+    it('getQuizData() should return a array for JSON data from a HTTP request', function() {
+
+    });
+
+    it('getScore() should return the correct score', function() {
+
+    });
+
+    it('updateScore() should increment the correct score to the score array', function() {
+
+    });
+
+    it('isValid() should valid the json data', function() {
+
+    });
+
+  });
 
 });
